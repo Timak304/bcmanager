174101287449aabdb72d54fac2165210205d3205
Timak <7514396+Timak304@users.noreply.github.com>
2025-11-23 16:49:33 +0100

    add i18n

--- src/main/app/src/main.tsx
+++ src/main/app/src/main.tsx
@@ -2,6 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from './i18n/en/global.json';
+import fr from './i18n/fr/global.json';
+
+i18next
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en: {
+        translation: en
+      },
+      fr: {
+        translation: fr
+      },
+    },
+    supportedLngs: ['en', 'fr'],
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false,
+    }
+  });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
